0c781ac96417baf7e941f70e94b5c6aa72d357be
JJ <jonathansimonjones@gmail.com>
2013-07-05 01:00:41 +0100

    Completed Chapter 4, machine code, had to write assembly

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -9,3 +9,79 @@
 // program clears the screen, i.e. writes "white" in every pixel.
 
 // Put your code here.
+(MAIN_LOOP)
+	@24576		// Address of keyboard
+	D=M
+	@MAKE_SCREEN_WHITE
+	D;JEQ		// If D is zero
+	@MAKE_SCREEN_BLACK
+	D;JGT		// If D is not zero
+(MAKE_SCREEN_BLACK)
+	// Init
+	@16384				
+	D=A
+	@ramStartAddress
+	M=D					// Set ramStartAddress
+	@8192
+	D=A
+	D=D-1
+	@counter
+	M=D					// Set counter
+	@BLACK_LOOP
+	0;JMP				// Go to black loop
+(BLACK_LOOP)
+	@counter
+	D=M
+	@MAIN_LOOP
+	//D;JEQ				// If D is zero return to MAIN_LOOP
+	D;JLT
+	@ramStartAddress
+	D=M
+	@counter
+	D=D+M				// Address to change
+	@address			
+	M=D					// Store that address
+	@0				
+	D=!A				// Make 
+	@address
+	//M=D
+	A=M
+	M=D
+	@counter
+	M=M-1
+	@BLACK_LOOP
+	0;JMP				// Go to black loop
+(MAKE_SCREEN_WHITE)
+	// Init
+	@16384				
+	D=A
+	@ramStartAddress
+	M=D					// Set ramStartAddress
+	@8192
+	D=A
+	D=D-1
+	@counter
+	M=D					// Set counter
+	@WHITE
+	0;JMP				// Go to black loop
+(WHITE)
+	@counter
+	D=M
+	@MAIN_LOOP
+	//D;JEQ				// If D is zero return to MAIN_LOOP
+	D;JLT
+	@ramStartAddress
+	D=M
+	@counter
+	D=D+M
+	@address
+	M=D
+	@0
+	D=A
+	@address
+	A=M
+	M=D
+	@counter
+	M=M-1
+	@WHITE
+	0;JMP				// Go to black loop
--- 04/mult/Mult.asm
+++ 04/mult/Mult.asm
@@ -7,3 +7,28 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[3], respectively.)
 
 // Put your code here.
+	@1
+	D=M			// Set D to hold val 1
+	@counter
+	M=D			// Set address of counter to A
+	@2
+	M=0
+(LOOP)
+	@counter	// load counter
+	D=M			// Set D to counter val
+	@END		// Set new branch
+	D;JLE		// Check D is not less than 0
+	@2			// load sum
+	D=M			// Set sum to D
+	@0			// load val 1
+	A=M			// set val 1 to D
+	D=D+A		// Add val 1 to D
+	@2			// load sum to A
+	M=D			// set new sum to sum
+	@counter	// load counter
+	M=M-1		// minus 1 off counter
+	@LOOP		// set branch
+	0;JMP		// Jump to loop
+(END)
+	@END
+	0;JMP		// Infinite loop
